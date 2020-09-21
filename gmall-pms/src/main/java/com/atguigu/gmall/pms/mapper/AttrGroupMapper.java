b8567b1ea875d78e0e5f235c1abded2de4c0d21d
liang <7902368+langdong8023@user.noreply.gitee.com>
2020-09-21 20:41:07 +0800

    完成pms模块

---
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/mapper/AttrGroupMapper.java
@@ -0,0 +1,17 @@
+package com.atguigu.gmall.pms.mapper;
+
+import com.atguigu.gmall.pms.entity.AttrGroupEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 属性分组
+ * 
+ * @author langD
+ * @email langD@atguigu.com
+ * @date 2020-09-21 20:11:35
+ */
+@Mapper
+public interface AttrGroupMapper extends BaseMapper<AttrGroupEntity> {
+	
+}
