5d1b362f2ad7387ae2b19866f55929b3315cc66d
liang <7902368+langdong8023@user.noreply.gitee.com>
2020-09-24 20:32:05 +0800

    day04完成

--- gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/SpuServiceImpl.java
+++ gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/SpuServiceImpl.java
@@ -1,7 +1,22 @@
 package com.atguigu.gmall.pms.service.impl;
 
+import com.atguigu.gmall.pms.entity.*;
+import com.atguigu.gmall.pms.feign.GmallSmsClient;
+import com.atguigu.gmall.pms.mapper.SkuMapper;
+import com.atguigu.gmall.pms.mapper.SpuDescMapper;
+import com.atguigu.gmall.pms.service.*;
+import com.atguigu.gmall.pms.vo.SkuVo;
+import com.atguigu.gmall.pms.vo.SpuAttrValueVo;
+import com.atguigu.gmall.pms.vo.SpuVo;
+import com.atguigu.gmall.sms.vo.SkuSaleVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.Map;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -10,12 +25,33 @@ import com.atguigu.gmall.common.bean.PageParamVo;
 import org.apache.commons.lang3.StringUtils;
 
 import com.atguigu.gmall.pms.mapper.SpuMapper;
-import com.atguigu.gmall.pms.entity.SpuEntity;
-import com.atguigu.gmall.pms.service.SpuService;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 
 @Service("spuService")
 public class SpuServiceImpl extends ServiceImpl<SpuMapper, SpuEntity> implements SpuService {
+
+    @Autowired
+    private SpuDescMapper descMapper;
+
+    @Autowired
+    private SpuAttrValueService baseAttrService;
+
+    @Autowired
+    private SkuMapper skuMapper;
+
+    @Autowired
+    private SkuImagesService imagesService;
+
+    @Autowired
+    private SkuAttrValueService attrValueService;
+
+    @Autowired
+    private GmallSmsClient smsClient;
+
+    @Autowired
+    private SpuDescService descService;
 
     @Override
     public PageResultVo queryPage(PageParamVo paramVo) {
@@ -51,5 +87,101 @@ public class SpuServiceImpl extends ServiceImpl<SpuMapper, SpuEntity> implements
 
         return new PageResultVo(page);
     }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class, timeout = 3, readOnly = true)
+    public void bigSave(SpuVo spu) {
+        //1.保存spu相关信息
+        // 1.1.保存pms_spu
+        spu.setCreateTime(new Date());
+        spu.setUpdateTime(spu.getCreateTime());
+        this.save(spu);
+        Long spuId = spu.getId();
+
+        // 1.2.保存pms_spu_desc
+        if (!CollectionUtils.isEmpty(spu.getSpuImages())){
+            SpuDescEntity spuDescEntity = new SpuDescEntity();
+            spuDescEntity.setSpuId(spuId);
+            spuDescEntity.setDecript(StringUtils.join(spu.getSpuImages(),","));
+            this.descMapper.insert(spuDescEntity);
+        }
+
+        // 1.3.保存pms_spu_attr_value
+        List<SpuAttrValueVo> baseAttrs = spu.getBaseAttrs();
+        if (!CollectionUtils.isEmpty(baseAttrs)){
+//            List<SpuAttrValueEntity> spuAttrValueEntities =null;
+//            baseAttrs.forEach(spuAttrValueVo -> {
+//                SpuAttrValueEntity spuAttrValueEntity = new SpuAttrValueEntity();
+//                BeanUtils.copyProperties(spuAttrValueVo, spuAttrValueEntity);
+//                spuAttrValueEntity.setSpuId(spuId);
+//                spuAttrValueEntity.setSort(0);
+//                spuAttrValueEntities.add(spuAttrValueEntity);
+//            })
+
+
+            this.baseAttrService.saveBatch(baseAttrs.stream().map(spuAttrValueVo ->{
+                SpuAttrValueEntity spuAttrValueEntity = new SpuAttrValueEntity();
+                BeanUtils.copyProperties(spuAttrValueVo, spuAttrValueEntity);
+                spuAttrValueEntity.setSpuId(spuId);
+                spuAttrValueEntity.setSort(0);
+                return spuAttrValueEntity;
+
+            }).collect(Collectors.toList()));
+        }
+
+        //2.保存sku相关信息
+        List<SkuVo> skus = spu.getSkus();
+        if(CollectionUtils.isEmpty(skus)){
+            return;
+        }
+        skus.forEach(skuVo -> {
+            // 2.1.保存pms_sku
+            skuVo.setSpuId(spuId);
+            skuVo.setCatagoryId(spu.getCategoryId());
+            skuVo.setBrandId(spu.getBrandId());
+
+            List<String> images = skuVo.getImages();
+            if(!CollectionUtils.isEmpty(images)){
+                skuVo.setDefaultImage(StringUtils.isNotBlank(skuVo.getDefaultImage()) ? skuVo.getDefaultImage(): images.get(0));
+            }
+            this.skuMapper.insert(skuVo);
+            Long skuId = skuVo.getId();
+
+            // 2.2.保存pms_sku_images
+            if(CollectionUtils.isEmpty(images)){
+                imagesService.saveBatch(images.stream().map(image -> {
+                    SkuImagesEntity skuImagesEntity = new SkuImagesEntity();
+                    skuImagesEntity.setSkuId(skuId);
+                    skuImagesEntity.setUrl(image);
+                    skuImagesEntity.setSort(0);
+                    if (StringUtils.equals(image, skuVo.getDefaultImage())){
+                        skuImagesEntity.setDefaultStatus(1);
+                    }
+                    return skuImagesEntity;
+                }).collect(Collectors.toList()));
+
+            }
+            // 2.3.保存pms_sku_attr_value
+            List<SkuAttrValueEntity> saleAttrs = skuVo.getSaleAttrs();
+            if (!CollectionUtils.isEmpty(saleAttrs)){
+                saleAttrs.forEach(attr -> {
+                    attr.setSkuId(skuId);
+                    attr.setSort(0);
+                });
+                this.attrValueService.saveBatch(saleAttrs);
+            }
+
+            // 3.保存sku营销信息
+            SkuSaleVo skuSaleVo = new SkuSaleVo();
+            BeanUtils.copyProperties(skuVo, skuSaleVo);
+            skuSaleVo.setSkuId(skuId);
+            this.smsClient.saveSales(skuSaleVo);
+        });
+
+    }
+
+
+
+
 
 }
